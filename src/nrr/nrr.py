1e4983d96fcf97024115ac8671bc5e353e21758d
alejandro-sanchez-gomez <jandro.sangom@gmail.com>
2024-04-10 12:27:04 +0200

    modified:   src/nrr/nrr.py

--- src/nrr/nrr.py
+++ src/nrr/nrr.py
@@ -1,4 +1,8 @@
 
+import os, io
+import datetime
+from azure.storage.blob import BlobServiceClient, BlobClient
+
 class NRR:
 
     def __init__(self, PCE, CP, GDP, INDPRO, USREC, SLRTTO01USQ661S, UNRATE, VIXCLS, daily_treasury_yield_curve):
@@ -50,10 +54,51 @@ class NRR:
     def get_YIELD(self):
         return self.YIELD
 
-    def calculate_nrr(self):
-        nrr = 5
-        self.nrr = nrr
+    def calculate_nrr_value(self):
+        nrr_value = 5
+        self.nrr_value = nrr_value
     
+    def download_nrr_azure(self):
+
+        #authentificate
+        connect_str = os.getenv('KEY_NRR_DATA')
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str) 
+
+        #client
+        blob = BlobClient(account_url="https://<account_name>.blob.core.windows.net"
+                  container_name="<container_name>",
+                  blob_name="<blob_name>",
+                  credential="<account_key>")
+
+        #data
+        with open("example.csv", "wb") as f:
+            data = blob.download_blob()
+            data.readinto(f)
+        
+        #csv to dataframe
+        self.nrr_table = data
+
+    def update_nrr_table(self):
+        #get month
+        current_month = datetime.now().month
+        current_month = str(current_month)
+
+    def upload_nrr_azure(self):
+
+        #authentificate
+        connect_str = os.getenv('KEY_NRR_DATA')
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+        #client
+        blob_client = blob_service_client.get_blob_client(container = "container-nrr", blob = "nrr_table")
+
+        #data to csv
+        writer = io.BytesIO()
+        self.nrr_table.to_csv(writer)
+        
+        #upload
+        blob_client.upload_blob(writer.getvalue(), overwrite = True)
+
     def get_nrr(self):
         return self.nrr
     
